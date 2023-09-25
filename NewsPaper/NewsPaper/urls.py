113b3862f22361103f060ec7f5e0e71345bf2e42
Kirill <kirill.voskresenov@yandex.ru>
2023-09-26 00:48:48 +0300

    update

--- NewsPaper/NewsPaper/urls.py
+++ NewsPaper/NewsPaper/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
-    path('news/', include('news.urls')),
-    path('post/', include('news.urls'))
+    path('news/', include('news.urls'))
 ]
 
